e894474c65ef929f7d2fd45ce0b0b04438b14c14
qjhjames <qiujunhong1990@126.com>
2017-05-09 17:27:33 +0800

    some update

---
+++ src/main/java/com/qianlikange/com/qianlikange/domain/Product.java
@@ -0,0 +1,12 @@
+package com.qianlikange.com.qianlikange.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by qiujunhong on 2017/5/9.
+ */
+@Entity
+@Table(name = "user")
+public class Product {
+}
